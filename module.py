a4cda922f7069b322d8c5d70a989b8f75ab1eb6c
DESKTOP-JMGR3K9\wrtop <wrtoppass@gmail.com>
2023-04-27 16:23:05 -0500

    first commit

---
+++ module.py
@@ -0,0 +1,16 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+    
+def adder(num1, num2):
+    return num1 + num2
+
+import math
+
+def circle_circumference(radius):
+    return 2 * math.pi * radius
+
+def house_square_footage(length, width):
+    return length * width
+
+
+
